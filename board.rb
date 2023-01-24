1a7f5c7683429a618fea7cdd876224f2d79803c1
abaez8031 <abaez8031@gmail.com>
2023-01-24 14:47:26 -0500

    working on board

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require_relative "card"
+
 class Board
   
   # def create_deck
@@ -36,21 +38,49 @@ class Board
 
 
   def populate
-    
     (0...@grid.length).each do |i|
       (0...@grid[i].length).each do |j|
         chosen = @deck.sample
-        @grid[i][j] = chosen
+        @grid[i][j] = Card.new(chosen)
         @deck.delete_at(@deck.index(chosen))
       end
     end
 
   end
 
+  def render
+    output_grid = []
+    @grid.each do |row|
+      new_row = []
+      row.each do |card|
+        if card.revealed? 
+          new_row << card.value
+        else
+          new_row << " "
+        end
+      end
+      output_grid << new_row
+    end
+    output_grid
+  end
+
+  def won?
+    @grid.each do |row|
+      row.each do |card|
+        return false if card.revealed? == false
+      end
+    end
+    true
+  end
+
+  def [](pos)
+    row,col = pos
+    @grid[row][col]
+  end
 
   
-
-
+  def reveal
+  end
 
 
 
